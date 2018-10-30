6affafd95a9adf99253067bd26c7d2170ee09afa
zengxiangchao <zengxiangchao@xiaomi.com>
2018-10-30 01:58:45 -0700

    reverse iter

--- vector/reverse_iter.cpp
+++ vector/reverse_iter.cpp
@@ -8,8 +8,15 @@ int main() {
     vector<int> nums(a, a + 5);
     
     sort(nums.begin(), nums.end());
-    for (int n : nums)
-        cout << n << " ";
+    //for (int n : nums)
+    //    cout << n << " ";
+    for (int i = 0; i < nums.size(); i ++)
+        cout << nums[i] << " ";
+    cout << endl;
+
+    vector<int>::reverse_iterator r_iter = nums.rbegin();
+    for (; r_iter != nums.rend(); r_iter ++)
+        cout << *r_iter << " ";
     cout << endl;
 
     return 0;
